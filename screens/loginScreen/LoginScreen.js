13ffcba5fd96cabd3d2c4afc668175b27156ecc2
ranyon <kwakumensahj@gmail.com>
2022-06-01 13:20:53 +0000

    added center element

---
+++ screens/loginScreen/LoginScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+
+
+export default function LoginScreen() {
+  return (
+      <View>
+        <center>
+        <Text>LoginScreen</Text>
+        </center>
+      </View>
+  )
+}
