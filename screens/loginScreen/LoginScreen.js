d2506e733bb4ae5474907af76a8a9dae2118b2ea
ranyon <kwakumensahj@gmail.com>
2022-06-06 11:33:31 +0000

    configuring createuser with email and password

--- screens/loginScreen/LoginScreen.js
+++ screens/loginScreen/LoginScreen.js
@@ -1,13 +1,37 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text, TextInput, TouchableOpacity} from 'react-native'
+import { useState } from 'react'
+import { authentication } from '../../components/firebaseConfig/firebase-config'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 
 export default function LoginScreen() {
+  const[email,setEmail]= useState('')
+  const[password,setPassword]= useState('')
+
+  const RegisterUser = ()=>{
+    createUserWithEmailAndPassword(authentication,email,password)
+    .then((re)=>{
+      console.log(re);
+    })
+    .catch((re)=>{
+      console.log
+    })
+    setEmail('')
+    setPassword('')
+  }
   return (
       <View>
         <center>
-        <Text>LoginScreen</Text>
+        <Text>Sign Up</Text>
         </center>
+        <TextInput placeholder='Email' value={email} onChangeText={text=>setEmail(text)}/>
+        <TextInput placeholder='Password'value={password} onChangeText={text=>setPassword(text)}/>
+        <TouchableOpacity onPress={RegisterUser}>
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
       </View>
   )
 }
+
+
