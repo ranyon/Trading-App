e1c5ed29b44be3901c70a4582fd4c6abe9169289
ranyon <kwakumensahj@gmail.com>
2022-06-07 19:58:33 +0000

    capitalized component

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/loginScreen/LoginScreen';
-import HomeScreen from './screens/homeScreen/homeScreen'
+import HomeScreen from './screens/homeScreen/HomeScreen'
 
 
 const Stack = createNativeStackNavigator();
