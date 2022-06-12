77afbc8d896a1702e6a0d48cceaf936aaad4d73e
ranyon <kwakumensahj@gmail.com>
2022-06-12 13:48:13 +0000

    added sign out

--- screens/homeScreen/Home.js
+++ screens/homeScreen/Home.js
@@ -1,11 +1,39 @@
 import React from 'react'
-import {View,Text} from 'react-native'
+import {View,Text, TouchableOpacity,StyleSheet} from 'react-native'
+import { authentication } from '../../components/firebaseConfig/firebase-config'
+import { useNavigation } from '@react-navigation/native'
+import { signOut } from 'firebase/auth'
+import { useEffect } from 'react'
 
 export default function Home() {
+
+  const navigation= useNavigation()
+
+  
+
+  const handleSignOut = async ()=>{
+    await signOut(authentication).then(()=>{
+      console.log("you are logged out")
+      navigation.navigate("Login")
+    })
+    .catch(error => alert(error.message))
+  }
   return (
-      <View>
+      <View style={styles.container}>
 
           <Text>Home</Text>
+          <TouchableOpacity onPress={handleSignOut}>
+            <Text>Sign Out</Text>
+          </TouchableOpacity>
       </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    marginHorizontal:10,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:70
+  }
+})
