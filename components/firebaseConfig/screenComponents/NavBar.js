832e6b96e976e9e24efa05711bea5fd95bd570ba
ranyon <kwakumensahj@gmail.com>
2022-06-16 20:14:33 +0000

    added style sheet

--- components/firebaseConfig/screenComponents/NavBar.js
+++ components/firebaseConfig/screenComponents/NavBar.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { View ,Text} from 'react-native'
-
+import { View ,Text,StyleSheet} from 'react-native'
+ 
 export default function NavBar() {
   return (
-    <View>
+    <View style={styles.container}>
         <Text>Tab navigation</Text>
     </View>
   )
 }
+
+const styles= StyleSheet.create({
+  container:{
+
+  }
+})
