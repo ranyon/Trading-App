016aacc800dd21862f76d635beb93ecf86027f24
ranyon <kwakumensahj@gmail.com>
2022-06-19 22:45:22 +0000

    copy and pasting content on home to profile screen

--- screens/ProfileScreen/ProfileScreen.js
+++ screens/ProfileScreen/ProfileScreen.js
@@ -1,16 +1,54 @@
 import React from 'react'
-import { View,Text ,StyleSheet} from 'react-native'
+import {View,Text, TouchableOpacity,StyleSheet} from 'react-native'
+import NavBar from '../../components/firebaseConfig/screenComponents/navBar'
+import { authentication } from '../../components/firebaseConfig/firebase-config'
+import { useNavigation } from '@react-navigation/native'
+import { signOut } from 'firebase/auth'
 
-export default function ProfileScreen() {
+
+//we will import firestore to firebase .config
+
+export default function Home() {
+
+  const navigation= useNavigation()
+
+  
+    // Sign Out Handler 
+    const handleSignOut = async ()=>{
+      await signOut(authentication).then(()=>{
+        console.log("you are logged out")
+        navigation.navigate("Login")
+      })
+      .catch(error => alert(error.message))
+      // Sign Out Handler 
+
+  }
   return (
-    <View style={styles.container}>
-        <Text>ProfileScreen</Text>
-    </View>
+      <View style={styles.container}>
+        {/* Sign Out fun  */}
+          <Text>Home</Text>
+          <TouchableOpacity onPress={handleSignOut}>
+            <Text>Sign Out</Text>
+          </TouchableOpacity>
+        {/* Sign Out fun  */}
+
+          {/* Tab Navigator */}
+          <View style={styles.navBarContainer}>
+            <NavBar/>
+          </View>
+          {/* Tab Navigator */}
+      </View>
   )
 }
 
-const styles= StyleSheet.create({
+const styles = StyleSheet.create({
   container:{
     marginHorizontal:10,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:70
+  },
+  navBarContainer:{
+    marginTop:600
   }
 })
