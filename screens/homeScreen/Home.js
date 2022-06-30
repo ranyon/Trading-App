1f7b9a132457b26ebcd87e05044196f7a6067cae
ranyon <kwakumensahj@gmail.com>
2022-06-30 21:40:41 +0000

    edit

--- screens/homeScreen/Home.js
+++ screens/homeScreen/Home.js
@@ -31,7 +31,7 @@ export default function Home() {
           <Text>Home</Text>
           <Text>Unit one</Text>
           <Text>Unit one</Text>
-          <Text>Unit one</Text>
+          
           
           
         </View>
