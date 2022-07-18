86ef0eb9c343961f2b63d438c54b282a9fa92bc9
ranyon <kwakumensahj@gmail.com>
2022-07-18 11:37:28 +0000

    trying to fix shopscreen

--- screens/shopScreen/ShopScreen.js
+++ screens/shopScreen/ShopScreen.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { View ,Text} from 'react-native'
+import {View,Text, TouchableOpacity,StyleSheet} from 'react-native'
+import NavBar from '../../components/firebaseConfig/screenComponents/navBar'
+import { useNavigation } from '@react-navigation/native'
+import { signOut } from 'firebase/auth'
+
+
+//we will import firestore to firebase .config
 
 export default function ShopScreen() {
-  return (
     <View>
         <Text>Shop</Text>
     </View>
-  )
+  
 }
+
+const styles = StyleSheet.create({
+  container:{
+    marginHorizontal:10,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:70
+  },
+  navBarContainer:{
+    marginTop:600
+  }
+})
