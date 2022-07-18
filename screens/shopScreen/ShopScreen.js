803d4ae0b6eadd02247e8dabd983d806f71ab63f
ranyon <kwakumensahj@gmail.com>
2022-07-18 19:17:06 +0000

    added shopscreen

--- screens/shopScreen/ShopScreen.js
+++ screens/shopScreen/ShopScreen.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import {View,Text, TouchableOpacity,StyleSheet} from 'react-native'
-import NavBar from '../../components/firebaseConfig/screenComponents/navBar'
-import { useNavigation } from '@react-navigation/native'
-import { signOut } from 'firebase/auth'
+import {View,Text,StyleSheet} from 'react-native'
+import NavBar from '../screenComponents/navBar'
+// import { useNavigation } from '@react-navigation/native'
+// import { signOut } from 'firebase/auth'
 
 
 //we will import firestore to firebase .config
 
 export default function ShopScreen() {
-    <View>
+    <View style={styles.container}>
         <Text>Shop</Text>
+        <View style={styles.navBarContainer}>
+            <NavBar/>
+        </View>
     </View>
   
 }
